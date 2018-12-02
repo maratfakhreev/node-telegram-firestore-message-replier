2a3b2cf5c024acbedd0a9710b2c910be855bcd75
Marat Fakhreev <marat.fakhreev@gmail.com>
2018-12-02 16:25:23 +0300

    Introduce library

---
+++ README.md
@@ -0,0 +1,80 @@
+## node-telegram-firestore-message-replier
+
+[![Build Status](https://travis-ci.org/maratfakhreev/node-telegram-firestore-message-replier.svg?branch=master)](https://travis-ci.org/maratfakhreev/node-telegram-firestore-message-replier)
+
+Node module which allows the bot to reply to a random message in the chat.
+
+You just set the probability between 1 and 100 and then the magic happens.
+
+### Install:
+
+```bash
+npm install node-telegram-firestore-message-replier
+```
+
+### Use:
+
+[First create your own firestore db instance](https://console.firebase.google.com)
+
+node-telegram-firestore-message-replier initially works with [node-telegram-bot-api](https://github.com/yagop/node-telegram-bot-api).
+
+```javascript
+const TelegramBot = require('node-telegram-bot-api');
+const TelegramFirestoreMessageReplier = require('node-telegram-firestore-message-replier');
+
+// initialize
+const bot = new TelegramBot('BOT_TOKEN');
+const replier = new TelegramFirestoreMessageReplier({
+  bot,
+  firestore: {
+    credential: require('./serviceKey.json'),
+    databaseUrl: 'https://YOUR_FIREBASE_URL.firebaseio.com',
+    docPath: 'collection/chats'
+  },
+  defaultChance: 0,
+  showChanceMessage: 'Current chance is CURRENT_CHANCE%',
+  setChanceMessage: 'Current chance changed from CURRENT_CHANCE% to NEXT_CHANCE%'
+  // CURRENT_CHANCE and NEXT_CHANCE strings will be replaced with currentChance and nextChance values
+});
+
+// on command "/chance" will show current chance value
+bot.onText(/^\/chance($|@)/, msg => {
+  replier.showChance(msg);
+});
+
+// on command "/setchance 30" will set chance value to 30% to reply to the message
+// command "/setchance" without value is equal to "/chance". So you can use only one command in your bot
+bot.onText(/^\/chance(@.* )?(.+)?/, (msg, match) => {
+  const chanceValue = match[2];
+
+  replier.setChance(msg, chanceValue);
+});
+
+// handle each message to be ready for replying
+bot.on('message', msg => {
+  // process() method calculates chance for message of being replied and calls one of the callbacks
+  // the chance is set previously via "/setchance <value>" command
+  replier.process(
+    msg,
+    repliedMsg => {
+      // success callback. Do something with lucky message
+      bot.sendMessage(repliedMsg.chat.id, repliedMsg);
+    },
+    repliedMsg => {
+      // error callback [optional]. Do something with unlucky message if you want
+    }
+  );
+});
+```
+
+### Options:
+
+```javascript
+new TelegramFirestoreMessageReplier({
+  bot: <your bot instance> // previously created bot via node-telegram-bot-api
+  firestore: <object> // firestore db instance params
+  defaultChance: <number> // default chance to reply to a message | default: 0
+  showChanceMessage: <string> // set message for show chance command
+  setChanceMessage: <string> // set message for change chance command
+});
+```
---
+++ index.js
@@ -0,0 +1,122 @@
+const { random, remove, unionWith } = require('lodash');
+const firebase = require('firebase-admin');
+
+module.exports = class TelegramFirestoreMessageReplier {
+  constructor(options) {
+    firebase.initializeApp({
+      credential: firebase.credential.cert(options.firestore.credential),
+      databaseURL: options.firestore.databaseUrl,
+    });
+
+    this.storage = firebase.firestore().doc(options.firestore.docPath || 'collection/chats');
+    this.bot = options.bot;
+    this.defaultChance = options.defaultChance || 0;
+    this.showChanceMessage = options.showChanceMessage || 'Current chance is CURRENT_CHANCE%';
+    this.setChanceMessage = options.setChanceMessage || 'Current chance changed from CURRENT_CHANCE% to NEXT_CHANCE%';
+  }
+
+  showChance(msg) {
+    const chatId = msg.chat.id;
+    const currentChance = this.getCurrentChance(chatId);
+
+    this.bot.sendMessage(chatId, this.handleMessage(this.showChanceMessage, currentChance));
+  }
+
+  setChance(msg, value) {
+    const chatId = msg.chat.id;
+    const chance = parseInt(value);
+
+    if (isNaN(chance)) {
+      const currentChance = this.getCurrentChance(chatId);
+
+      return this.bot.sendMessage(chatId, this.handleMessage(this.showChanceMessage, currentChance));
+    }
+
+    this.saveChance(chatId, chance);
+  }
+
+  process(msg, successCb, errorCb = () => {}) {
+    const chatId = msg.chat.id;
+    const currentChatIndex = this.getChatIndex(chatId);
+
+    if (!successCb || typeof successCb !== 'function') {
+      throw new Error('Specify success callback that handles replied message as second parameter of process function');
+    }
+
+    if (currentChatIndex > -1) {
+      if (random(1, 100) <= this.getChat(currentChatIndex).chance) {
+        successCb(msg);
+      } else {
+        errorCb(msg);
+      }
+    } else {
+      errorCb(msg);
+    }
+  }
+
+  handleMessage(msg, currentChance = null, nextChance = null) {
+    let newMsg = msg;
+
+    if (currentChance !== null) newMsg = newMsg.replace(/CURRENT_CHANCE/g, currentChance);
+    if (nextChance !== null) newMsg = newMsg.replace(/NEXT_CHANCE/g, nextChance);
+
+    return newMsg;
+  }
+
+  saveChance(chatId, nextChance) {
+    if (nextChance > 100) nextChance = 100;
+
+    const currentChance = this.getCurrentChance(chatId);
+
+    this.bot.sendMessage(chatId, this.handleMessage(this.setChanceMessage, currentChance, nextChance));
+
+    if (nextChance === 0) {
+      this.removeChat(chatId);
+    } else {
+      this.pushChat({ chatId, chance: nextChance });
+    }
+  }
+
+  getCurrentChance(chatId) {
+    const currentChatIndex = this.getChatIndex(chatId);
+
+    return currentChatIndex > -1 ?
+      this.getChat(currentChatIndex).chance :
+      this.defaultChance;
+  }
+
+  async getChats() {
+    const chats = await this.storage.get();
+
+    return chats.data().ids || [];
+  }
+
+  setChats(chats) {
+    this.storage.set({ ids: chats });
+  }
+
+  getChat(index) {
+    return this.getChats()[index];
+  }
+
+  getChatIndex(chatId) {
+    const chats = this.getChats();
+    const chat = chats.filter(chat => chat.chatId === chatId)[0];
+
+    return chats.indexOf(chat);
+  }
+
+  removeChat(chatId) {
+    const chats = this.getChats();
+
+    remove(chats, item => item.chatId === chatId);
+    this.setChats(chats);
+  }
+
+  pushChat(data) {
+    const chats = this.getChats();
+    const unionChats = unionWith([data], chats, (a, b) => a.chatId === b.chatId);
+
+    this.setChats(unionChats);
+  }
+};
